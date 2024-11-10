6df5ff45dc73a01aa0878c4591a42009440a93bc
Michael Marconi <michaelziga2@gmail.com>
2024-11-10 17:12:26 -0600

    settin more up

---
+++ public/backend/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bcrypt = require('bcrypt');
+const dbConfig = require('./dbConfig');
+require('dotenv').config();
+
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(cors());
+
+require('dotenv').config();
+
+
+
+
+
+const GMAIL_USER = process.env.GMAIL_USER;
+const GMAIL_PASS = process.env.GMAIL_PASS;
+const RECIPIENT_EMAIL = process.env.RECIPIENT_EMAIL;
+
+
+
+// Endpoint to handle form submissions
+app.post("/api/send-email", async (req, res) => {
+    const { name, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: GMAIL_USER,
+            pass: GMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: GMAIL_USER,
+        to: RECIPIENT_EMAIL,
+        subject: "New Message from Your Website",
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.send("Email sent successfully");
+    } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending email");
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
